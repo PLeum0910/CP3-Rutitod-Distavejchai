60a2e0b7596d65efe7a68a949aa4fbc79c5722fe
Rutitod Distavejchai <88881697+PLeum0910@users.noreply.github.com>
2023-03-27 15:24:35 +0700

    Add files via upload

---
+++ Exercise4_Rutitod_D.py
@@ -0,0 +1,9 @@
+engs = 50
+buss = 60
+coms = 70
+pros = 50
+print("--- Your Score ---")
+print("Foundation English :",engs,"points")
+print("General Business :",buss,"points")
+print("Introduction to Computer Systems :",coms,"points")
+print("Computer Programming :",pros,"points")
