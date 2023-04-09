7dba89ee7eb0eee6f1d95e712c8c035d61ac9a73
Rutitod Distavejchai <88881697+PLeum0910@users.noreply.github.com>
2023-04-09 16:08:34 +0700

    Add files via upload

---
+++ Lecture50_Rutitod_D.py
@@ -0,0 +1,12 @@
+def plusnumber(x,y):
+    print(x+y)
+plusnumber(10,9)
+def minusnumber(x,y):
+    print(x-y)
+minusnumber(10,9)
+def multiplynumber(x,y):
+    print(x*y)
+multiplynumber(10,9)
+def dividenumber(x,y):
+    print(x/y)
+dividenumber(10,9)
