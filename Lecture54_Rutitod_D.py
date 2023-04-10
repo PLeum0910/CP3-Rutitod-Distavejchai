c4ccc98c341f1550cf82b3b5be13a246419b02e8
Rutitod Distavejchai <88881697+PLeum0910@users.noreply.github.com>
2023-04-10 14:01:00 +0700

    Add files via upload

---
+++ Lecture54_Rutitod_D.py
@@ -0,0 +1,36 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return showMenu()
+    else:
+        return login()
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    return menuSelect()
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected ==1:
+        return vatCalculator()
+    elif userSelected == 2:
+        return priceCalculator()
+    else:
+        return userSelected
+
+def vatCalculator():
+    totalPrice = int(input("Enter Price : "))
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    result = price1+price2
+    return result
+
+print(login())
