e7ccc1d1e004982765d42a80b1b25140bbea77c3
Rutitod Distavejchai <88881697+PLeum0910@users.noreply.github.com>
2023-05-13 20:00:54 +0700

    Add files via upload

---
+++ Exercise21_Rutitod_D.py
@@ -0,0 +1,41 @@
+from tkinter import *
+import math
+
+def leftclickbutton(event):
+    result = (float(textboxweight.get())/math.pow(float(textboxheight.get())/100,2))
+    labelresult.configure(text=result)
+    if result >= 30:
+        text_result.configure(text="อ้วนมาก")
+    elif result >= 25:
+        text_result.configure(text="อ้วน")
+    elif result >= 23:
+        text_result.configure(text="น้ำหนักเกิน")
+    elif result >= 18.6:
+        text_result.configure(text="น้ำหนักปกติ")
+    elif result <= 18.5:
+        text_result.configure(text="ผอมเกินไป")
+
+Mainwindow = Tk()
+labelheight = Label(Mainwindow,text = "ส่วนสูง (cm)")
+labelheight.grid(row=0,column=0)
+
+textboxheight = Entry(Mainwindow)
+textboxheight.grid(row=0,column=1)
+
+labelweight = Label(Mainwindow,text = "น้ำหนัก (kg)")
+labelweight.grid(row=1,column=0)
+
+textboxweight = Entry(Mainwindow)
+textboxweight.grid(row=1,column=1)
+
+calculatebutton = Button(Mainwindow,text = "ตำนวณ")
+calculatebutton.bind('<Button-1>',leftclickbutton)
+calculatebutton.grid(row=2)
+
+labelresult = Label(Mainwindow,text= "ผลลัพธ์")
+labelresult.grid(row=2,column=1)
+
+text_result = Label(Mainwindow,text =" ")
+text_result.grid(row=3,column=1)
+
+Mainwindow.mainloop()
